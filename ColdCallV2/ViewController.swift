39af5896faed0555099308338e4310854dfd14b9
Hassan Al-Shammari <hsn999hsn@gmail.com>
2021-12-01 13:45:34 +0000

    Initial Commit

---
+++ ColdCallV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColdCallV2
+//
+//  Created by administrator on 01/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

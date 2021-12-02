ece47e23bb18d9b74a632faf4147e6172be59174
Hassan Al-Shammari <hsn999hsn@gmail.com>
2021-12-02 03:58:51 +0000

    new project

--- ColdCallV2/ViewController.swift
+++ ColdCallV2/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  ColdCallV2
+//  Cold Call V2
 //
 //  Created by administrator on 01/12/2021.
 //
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let namesArray: [String] =
+    ["Hassan", "Amer", "Hamza","Tariq", "Sati"]
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func coldCallButton(_ sender: UIButton) {
+        changeNameAndNumber()
+    }
+    
+    private func getRandomNumber() -> Int{
+        return Int.random(in: 0..<5)
+    }
+    
+    private func changeNameAndNumber(){
+        let number = getRandomNumber()
+        nameLabel.text = namesArray[number]
+        numberLabel.text = String(number+1)
+        
+        switch number+1 {
+        case 1...2:
+            numberLabel.textColor = .red
+        case 3...4:
+            numberLabel.textColor = .orange
+        case 5:
+            numberLabel.textColor = .green
+        default:
+            numberLabel.textColor = .black
+        }
+    }
 }
 
